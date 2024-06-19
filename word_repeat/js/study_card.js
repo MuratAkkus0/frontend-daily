558c9c6554303c4539e228e671e7dd29a7482877
Murat Akkus <akkusmurat123@gmail.com>
2024-06-19 15:38:04 +0200

    Eklemelerden sonra olusan hatalar cözüldü

--- word_repeat/js/study_card.js
+++ word_repeat/js/study_card.js
@@ -9,8 +9,9 @@ const card_turn_btn = document.querySelector(".turn-btn");
 const card_prev_btn = document.querySelector(".btn-left");
 const card_next_btn = document.querySelector(".btn-right");
 // variable defination
-let category = JSON.parse(sessionStorage.getItem('config'));
+let words;
 let randomIndex;
+let prevCount = 1;
 
 addEventListener();
 initializeCard();
@@ -21,22 +22,28 @@ function addEventListener() {
     card_prev_btn.addEventListener('click', getPrevCard);
     card_turn_btn.addEventListener('click', turnCard);
 }
+// clearing the session and local storage
+sessionStorage.removeItem('usedNumbers');
+sessionStorage.removeItem('prevIndex');
 
 // Initializing Card
 function initializeCard() {
     let config = JSON.parse(sessionStorage.getItem('config'));
-    let words = getItemsFromLocalStorage();
-    words = filterCategory(config.category,words)
-    createRandomNumber(words)
-    let randomWord = words[randomIndex];
-    card_front_face.innerHTML = randomWord.word;
-    card_back_face.innerHTML = randomWord.word_tr;
+    words = getItemsFromLocalStorage();
+    if (words.length !== 0) {
+        words = filterCategory(config.category, words)
+        createRandomNumber(words)
+        let randomWord = words[randomIndex];
+        card_front_face.innerHTML = randomWord.word;
+        card_back_face.innerHTML = randomWord.word_tr;
+    }
 }
 
-function filterCategory(category,words){
+function filterCategory(category, words) {
     let filteredWords = [];
-    if (category === null) {return filteredWords}
-    for (let i = 0; i < words.length; i++){
+    if (category === null) { return filteredWords }
+    else if (category === 'mix') { return words }
+    for (let i = 0; i < words.length; i++) {
         if (words[i].word_type === category) {
             filteredWords.push(words[i]);
         }
@@ -47,44 +54,75 @@ function filterCategory(category,words){
 // Create Random Index Number
 function createRandomNumber(words) {
     let usedNumbers;
-    let prevIndex = randomIndex;
-    randomIndex = Math.floor(Math.random() * words.length);
-    // All Cards are used
+    let newIndex;
+    let prevIndex = JSON.parse(sessionStorage.getItem('prevIndex'));
+    // Initialize Random Index Number
+    if (words.length === 1 || words.length === 0) { randomIndex = 0; return}
+    else { newIndex = Math.floor(Math.random() * words.length) };
+
+    // Session Storage Control
     if (sessionStorage.getItem('usedNumbers') === null) {
         usedNumbers = [];
     } else {
         usedNumbers = JSON.parse(sessionStorage.getItem('usedNumbers'));
     }
+    checkDuplicate();
+
     // Duplicate Index Number Control
-    if  (usedNumbers.includes(randomIndex)) {
-        if (usedNumbers.length === words.length)    {
-            sessionStorage.removeItem('usedNumbers'); 
-            if (prevIndex !== null) {
-                sessionStorage.setItem('usedNumbers', JSON.stringify([prevIndex]));
+    function checkDuplicate() {
+        if (usedNumbers.includes(newIndex) || newIndex === randomIndex) {
+            if (usedNumbers.length === words.length) {
+                sessionStorage.removeItem('usedNumbers');
             }
+            return createRandomNumber(words);
+        } else {
+            prevIndex = randomIndex;
+            randomIndex = newIndex;
+            usedNumbers.push(randomIndex);
+            console.log(usedNumbers);
+            sessionStorage.setItem('usedNumbers', JSON.stringify(usedNumbers));
+            sessionStorage.setItem('prevIndex', JSON.stringify([prevIndex]))
         }
-        createRandomNumber(words);
-    }else{
-        randomIndex = randomIndex;
-        usedNumbers.push(randomIndex);
-        sessionStorage.setItem('usedNumbers', JSON.stringify(usedNumbers));
+
     }
+
+
+
 }
 
 function getNextCard(e) {
+    prevCount = 1;
     if (!isFirstClick) {
         turnCard()
         setTimeout(() => {
             initializeCard();
-        },200)
-    }else{
+        }, 200)
+    } else {
         initializeCard();
     }
-    
 
-}   
 
-function getPrevCard(e) {    
-    console.log('prev card')    
+}
+
+function getPrevCard(e) {
+    let usedNumbers = JSON.parse(sessionStorage.getItem('usedNumbers'));
+    if (usedNumbers === null) { return }
+    if (prevCount >= usedNumbers.length) { prevCount = 2 }
+    else { prevCount++ };
+    let prevIndex = usedNumbers[usedNumbers.length - prevCount];
+    let prevWord = words[prevIndex];
+
+
+
+    if (usedNumbers.length > 1) {
+        usedNumbers.splice(usedNumbers.length - prevCount, 1);
+        usedNumbers.push(prevIndex);
+        console.log(usedNumbers);
+        sessionStorage.setItem('usedNumbers', JSON.stringify(usedNumbers));
+    }
+    if (typeof (prevIndex) === 'number') {
+        card_front_face.innerHTML = prevWord.word;
+        card_back_face.innerHTML = prevWord.word_tr;
+    } else { return };
 }
 
