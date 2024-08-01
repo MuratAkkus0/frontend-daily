3ac9d4c926d8a8d889637f5e7893eef6a7d21eee
Murat Akkus <akkusmurat123@gmail.com>
2024-08-01 16:22:55 +0200

    mobile menu updated

--- word_repeat/js/main.js
+++ word_repeat/js/main.js
@@ -4,6 +4,7 @@ import { activeTracker } from "./active_tracker.js";
 let toolbarItems = document.querySelectorAll('.menu-item');
 let mobileMenu = document.querySelector('.mobile-menu-icon');
 let toolbarContainer = document.querySelector('.toolbar-container');
+let menu = document.querySelector('.menu');
 
 toolbarItems.forEach(item => {
     item.addEventListener('click', activeTracker);
@@ -12,22 +13,7 @@ toolbarItems.forEach(item => {
 mobileMenu.addEventListener('click', mobileMenuActive);
 
 function mobileMenuActive(e) {
-    toolbarContainer.classList.toggle('mobile-menu_active');
-
-    // e = e.target.parentElement;
-    // e.nextElementSibling.classList.remove('menu');
-    // void e.nextElementSibling.offsetWidth;
-    // e.nextElementSibling.classList.add('menu');
-    // if (e.target.classList.contains('menu-animation')) {
-    //     e.target.classList.remove('menu-animation');
-    // } else {
-    //     e.target.classList.add('menu-animation');
-    // }
-
-
-    // e.target.classList.remove('mobile-menu_animation');
-
-    // e.target.classList.add('mobile-menu_animation');    
+    menu.classList.toggle('active');
 }
 
 export { toolbarItems };
