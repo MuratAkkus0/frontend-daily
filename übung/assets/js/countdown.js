ad96b40f9bbe7d9a05d6e8a3ef72e0dc3ad3f7c7
Murat Akkus <akkusmurat123@gmail.com>
2024-08-08 13:14:08 +0200

    finish

--- übung/assets/js/countdown.js
+++ übung/assets/js/countdown.js
@@ -1,7 +1,7 @@
 const countdown_container = document.querySelector(".countdown_container");
 const remain_second = document.querySelector(".remain_second");
 let progressDeg = 0;
-const countVal = 30;
+const countVal = 15;
 const incrementDeg = 360 / countVal;
 let countInterval;
 remain_second.textContent = countVal;
@@ -14,6 +14,18 @@ function startCountdown() {
             countdown_container.style.background = `conic-gradient(red ${progressDeg}deg, #ddd ${progressDeg}deg)`
             remain_second.textContent = countVal - Math.floor(progressDeg / 360 * countVal);
         }
+        if (remain_second.textContent == 0) {
+            console.log("game over")
+            const popup = document.querySelector(".game_popup");
+            const popupContent = document.querySelector(".popup_content");
+            const score = document.querySelector(".score:not([data-id='tony'])");
+            const resetBtn = document.querySelector(".reset_btn");
+            popupContent.innerHTML = `Game Over! </br> Score: ${score.textContent}`;
+            popup.style.display = "flex";
+            resetBtn.dispatchEvent(new Event('click'));
+            const inputs = document.querySelectorAll(".answer_input");
+            inputs.forEach((input) => {input.disabled = true;});
+        }
     }, (1000));
 }
 function resetCountdown() {
@@ -24,4 +36,4 @@ function resetCountdown() {
     countdown_container.style.background = `conic-gradient(red ${progressDeg}deg, #ddd ${progressDeg}deg)`
 }
 
-export { startCountdown , countInterval , resetCountdown};
+export { startCountdown, countInterval, resetCountdown };
--- übung/assets/js/script.js
+++ übung/assets/js/script.js
@@ -1,10 +1,13 @@
 import { startCountdown, countInterval, resetCountdown } from "./countdown.js";
-import { getApiData, apiResult } from "./apiConnection.js";
+import { getApiData, apiResult, llmActive } from "./apiConnection.js";
 
 const answerInput = document.querySelectorAll(".answer_input");
 const answerBtn = document.querySelectorAll(".answer_btn");
+const startBtn = document.querySelector(".start_btn");
+const stopBtn = document.querySelector(".stop_btn");
 const popup = document.querySelector(".game_popup");
 const popupText = document.querySelector(".popup_text");
+const points = document.querySelectorAll(".score");
 let countControl = false;
 let prompt = `You are playing a game called Word Wars. You are tasked to respond as Tony Stark. Your response should be the chosen word. Do not use indicators like "Start" or "Speak" .The rules are as follows:
 When "Speak" is said, introduce yourself in a cool way and challenge your opponent.
@@ -13,30 +16,45 @@ When "Start" is said, you can only respond with a single word.
 Each letter in the word will receive points equal to its position in the alphabet (e.g., A = 1 point, Z = 26 points). `
 
 // Event Listeners
-const startBtn = document.querySelector(".start_btn").addEventListener("click", countControlFunc);
-const stopBtn = document.querySelector(".stop_btn").addEventListener("click", countControlFunc);
+startBtn.addEventListener("click", countControlFunc);
+stopBtn.addEventListener("click", countControlFunc);
 const resetBtn = document.querySelector(".reset_btn").addEventListener("click", countControlFunc);
+const popupBtn = document.querySelector(".popup_btn").addEventListener("click", closePopup);
+answerBtn.forEach((btn) => { btn.addEventListener("click", GetAnswer); })
+answerInput.forEach((input) => { input.addEventListener("keyup", GetAnswer); });
+answerInput.forEach((input) => { input.addEventListener("focus", (e) => {let event = new Event('click'); startBtn.dispatchEvent(event); }); });
+
 
 (async () => {
     await getApiData(prompt);
-        popupText.textContent = apiResult.llm_output;
-        popup.style.display = "flex";
-
+    popupText.textContent = apiResult.llm_output;
+    popup.style.display = "flex";
     console.log(apiResult)
 })()
 
+// Popup Control
+function closePopup() {
+    popup.style.display = "none";
+    answerInput.forEach((input) => { if (input.dataset.id != 'tony') {input.disabled=false;} });
+}
 // Getting Answer 
-answerInput.forEach((input) => {
-    input.addEventListener("keyup", (e) => {
-        if (e.key === "Enter") {
-            apiOptions.body.text = input.value;
-            // console.log(apiOptions.body.text)
-            // getApiData();
+function GetAnswer(e) {
+    //Event Type Kontrol
+    if ((e.type == "keyup" && e.key === "Enter") || e.type == "click" ) {
+        if (llmActive) {
+            let event = new Event('click');
+            stopBtn.dispatchEvent(event);
         }
-    })
-})
-// Count Control
+        let scoreArea = e.target.closest(".player_box").children[1].children[0].children[0];
+        let input = e.target.parentElement.children[0];
+        scoreArea.textContent = Number(scoreArea.textContent) + Aufgabe(input.value);
+        getApiData(input.value);
+        input.value = "";
+    }
+}
 
+
+// Count Control
 function countControlFunc(e) {
     e.stopPropagation();
     if (e.target.classList.contains("start_btn") && !countControl) {
@@ -50,18 +68,19 @@ function countControlFunc(e) {
     if (e.target.classList.contains("reset_btn")) {
         countControl = false;
         resetCountdown();
+        points.forEach((point) => { point.textContent = 0; });
+        answerInput.forEach((input) => { input.value = ""; });
     }
 }
 // Count Control End
 
-//             ******** Aufgabe ******** 
-function Aufgabe() {
+//                                      ******** Aufgabe ******** 
+function Aufgabe(text) {
     let result = 0;
     let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-    let inputWord = "test";
-    let inputWordArray = (inputWord.toLowerCase()).split("");
-
+    let inputWordArray = (text.toLowerCase()).split("");
     inputWordArray.forEach((letter) => {
         result += (alphabet.indexOf(letter) + 1);
     })
+    return result;
 }
