639368ba03cc42140163be989c5a7b05db8e3648
Murat Akkus <you@example.com>
2024-09-20 17:07:34 +0200

    VueJs notlari

---
+++ vue-lesson-notes/src/components/VForDöngüler.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { reactive } from 'vue';
+
+const actors = reactive([
+    { name: 'Robert Downey Jr.', age: 55, id: 1 },
+    { name: 'Tom Hanks', age: 47 , id: 2},
+    { name: 'Will Smith', age: 50 , id: 3},
+    { name: 'Denzel Washington', age: 58 , id: 4},
+    { name: 'Tom Cruise', age: 48 , id: 5},
+    { name: 'Samuel L. Jackson', age: 51 , id: 6},
+])
+</script>
+
+<template >
+    <div style="border:1px solid black" v-for="(actor,index) in actors" >
+        <p v-for="actorProps in actor">{{ actorProps }}</p>
+    </div>
+</template>
---
+++ vue-lesson-notes/src/components/VIf_VShow_Farki.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { reactive } from 'vue';
+
+/*
+--- v-if yapisi ile v-show yapisi arasindaki fark, v-show kosul saglansa da saglanmasada elemani HTML e ekler ve o elemana inline style olarak display none verir. v-if yapisi da eger kosul saglaniyorsa elemani HTML yapisina ekler , kosul saglanmiyorsa eleman HTML e eklenmez. Yani incele denince htmlde görünmez.
+*/
+
+const isSuccessful = reactive({
+    error:false
+})
+
+</script>
+
+<template>
+    <p v-if="isSuccessful.error">Hata!</p>
+
+    <p v-show="isSuccessful.error">v-show : Hata</p>
+
+    <button v-on:click="isSuccessful.error = !isSuccessful.error">Change Process Status</button>
+</template>
